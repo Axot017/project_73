362b350638d7bb4fc570f9b58eeca077bfeafb70
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-10-06 20:03:26 +0200

    feat: replace modal styles with dark mode styles

--- lib/project_73/profile/stripe_connector.ex
+++ lib/project_73/profile/stripe_connector.ex
@@ -30,6 +30,9 @@ defmodule Project73.Profile.StripeConnector do
            Stripe.PaymentIntent.create(%{
              amount: amount,
              currency: "PLN",
+             payment_method_options: %{
+               p24: %{}
+             },
              customer: customer_id,
              metadata: %{
                "id" => user_id
--- lib/project_73_web/live/wallet_live.ex
+++ lib/project_73_web/live/wallet_live.ex
@@ -3,6 +3,8 @@ defmodule Project73Web.WalletLive do
   alias Project73.Profile
   use Project73Web, :live_view
 
+  @stripe_public_key System.get_env("STRIPE_PUBLISHABLE_KEY")
+
   def mount(_params, %{"current_user" => profile}, socket) do
     Logger.debug("Profile loaded: #{inspect(profile)}")
     {:ok, pid} = Profile.Supervisor.get_actor(profile.id)
@@ -11,7 +13,8 @@ defmodule Project73Web.WalletLive do
      socket
      |> assign(
        current_user: profile,
-       actor_pid: pid
+       actor_pid: pid,
+       stripe_public_key: @stripe_public_key
      )}
   end
 
@@ -20,14 +23,35 @@ defmodule Project73Web.WalletLive do
     {:ok, deposit_data} = Profile.Actor.request_deposit(pid, Decimal.new("1000.00"))
     Logger.debug("Deposit data: #{inspect(deposit_data)}")
 
-    {:noreply, socket}
+    {:noreply,
+     socket
+     |> push_event("start_deposit", %{client_secret: deposit_data.client_secret})}
   end
 
   def render(assigns) do
     ~H"""
+    <script>
+      window.addEventListener("phx:start_deposit", (event) => {
+        console.log("phx:start_deposit", event.detail);
+        const stripe = Stripe("<%= @stripe_public_key %>");
+        const clientSecret = event.detail.client_secret;
+        
+        stripe.confirmPayment({
+          clientSecret: clientSecret,
+          confirmParams: {
+            return_url: "http://localhost:4000",
+          }
+        }).then((result) => {
+            console.log(result);
+        });
+      });
+    </script>
     <div>
       <%= Decimal.to_string(@current_user.wallet_balance) %>
     </div>
+    <.modal id="test-modal" show={true}>
+      Test modal
+    </.modal>
     <.button phx-click="deposit">Deposit</.button>
     """
   end
