c66dbe1e3e8528cd1c4d0f4404547f2bea240622
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-10-08 18:38:28 +0200

    refactor: clean up router

--- lib/project_73_web/router.ex
+++ lib/project_73_web/router.ex
@@ -18,44 +18,34 @@ defmodule Project73Web.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api", Project73Web do
+    pipe_through :api
+
+    scope "/v1", Project73Web do
+    end
+  end
+
   scope "/", Project73Web do
     pipe_through :browser
 
     get "/", PageController, :home
-
     get "/login", PageController, :login
-
     live "/auction", AuctionLive
+
+    scope "/auth" do
+      delete "/logout", AuthController, :delete
+
+      get "/:provider", AuthController, :request
+      get "/:provider/callback", AuthController, :callback
+    end
+
+    scope "/profile" do
+      live "/update", ProfileUpdateLive
+      live "/wallet", WalletLive
+    end
   end
 
-  scope "/auth", Project73Web do
-    pipe_through :browser
-
-    delete "/logout", AuthController, :delete
-
-    get "/:provider", AuthController, :request
-    get "/:provider/callback", AuthController, :callback
-  end
-
-  scope "/profile", Project73Web do
-    pipe_through :browser
-
-    live "/update", ProfileUpdateLive
-    live "/wallet", WalletLive
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Project73Web do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:project_73, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
