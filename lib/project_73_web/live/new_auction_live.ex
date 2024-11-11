e7e1ffd6c5557750acc5c913eddece48f9e07815
Mateusz Ledwoń <mateuszledwon@duck.com>
2024-11-11 14:23:54 +0100

    feat: add fields to new auction page

--- lib/project_73_web/live/new_auction_live.ex
+++ lib/project_73_web/live/new_auction_live.ex
@@ -3,12 +3,49 @@ defmodule Project73Web.NewAuctionLive do
   use Project73Web, :live_view
 
   def mount(_params, session, socket) do
-    {:ok, assign(socket, current_user: session["current_user"])}
+    {:ok,
+     socket
+     |> assign(
+       current_user: session["current_user"],
+       form:
+         to_form(%{
+           title: "",
+           description: "",
+           initial_price: ""
+         })
+     )
+     |> allow_upload(:images, accept: ~w(.jpg .jpeg .png), max_entries: 5)}
   end
 
   def render(assigns) do
     ~H"""
-    <div></div>
+    <div>
+      <.simple_form for={@form} phx-submit="save" phx-change="validate" class="space-y-6">
+        <.input
+          label="Title"
+          field={@form[:title]}
+          class="bg-gray-700 text-gray-100 border-gray-600 focus:border-indigo-500 focus:ring-indigo-500"
+        />
+        <.input
+          label="Description"
+          field={@form[:description]}
+          class="bg-gray-700 text-gray-100 border-gray-600 focus:border-indigo-500 focus:ring-indigo-500"
+        />
+        <.input
+          label="Initial Price"
+          type="number"
+          field={@form[:initial_price]}
+          class="bg-gray-700 text-gray-100 border-gray-600 focus:border-indigo-500 focus:ring-indigo-500"
+        />
+        <.live_file_input upload={@uploads.images} />
+        <.button
+          type="submit"
+          class="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-4 rounded-md transition duration-300 ease-in-out"
+        >
+          Creaet Auction
+        </.button>
+      </.simple_form>
+    </div>
     """
   end
 end
